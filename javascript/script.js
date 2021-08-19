85518502d0e7d45f77d8e121ffb529955ad9ec8c
André Santos <andredupmd@gmail>
2021-08-19 11:12:10 -0300

    Add fist development for js

--- javascript/script.js
+++ javascript/script.js
@@ -1,100 +1,80 @@
+//Aparecer elementos
 
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
 
-//buttons
-const btndark1 = document.getElementById('btn-dark1');
-
-btndark1.addEventListener('click', () => window.scrollTo({
-  top: 851,
-  behavior: 'smooth',
-}));
-const btndark2 = document.getElementById('btn-dark2');
-
-btndark2.addEventListener('click', () => window.scrollTo({
-  top: 851,
-  behavior: 'smooth',
-}));
-
-
-const btnprimary1 = document.getElementById('btn-primary1');
-
-btnprimary1.addEventListener('click', () => window.scrollTo({
-  top: 3400,
-  behavior: 'smooth',
-}));
-const btnprimary2 = document.getElementById('btn-primary2');
-
-btnprimary2.addEventListener('click', () => window.scrollTo({
-  top: 3400,
-  behavior: 'smooth',
-}));
-
-
-
-//projetos
-let interval0 = 0;
-let maxSlider0 = document.querySelectorAll('.box-imagem0').length - 1;
-acao0();
-
-function acao0(){
-
-  let img = document.querySelectorAll('.box-imagem0 img');
-
-  setInterval(function(){
-
-    img[interval0].style.display = 'none';
-    interval0++;
-    if(interval0 > maxSlider0){
-
-      interval0 = 0
+function animeScroll(){
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
+  target.forEach(function(element){
+    if((windowTop) > element.offsetTop){
+      element.classList.add(animationClass);
+    }else{
+      element.classList.remove(animationClass)
     }
-    img[interval0].style.display = 'block'
-
-  },2000)
-
+    
+    
+  })
 }
 
+animeScroll()
 
-let interval1 = 0;
-let maxSlider1 = document.querySelectorAll('.box-imagem1').length - 1;
-acao1();
-
-function acao1(){
-
-  let img = document.querySelectorAll('.box-imagem1 img');
-
-  setInterval(function(){
-
-    img[interval1].style.display = 'none';
-    interval1++;
-    if(interval1 > maxSlider1){
-
-      interval1 = 0
-    }
-    img[interval1].style.display = 'block'
-
-  },2000)
-
+if(target.length){
+  window.addEventListener('scroll', function(){
+    animeScroll();
+  });
 }
 
+//Scroll para elementos
 
-let interval2 = 0;
-let maxSlider2 = document.querySelectorAll('.box-imagem2').length - 1;
-acao2();
+const menuItems = document.querySelectorAll('.navbar-nav a[href^="#"]');
 
-function acao2(){
+menuItems.forEach(item =>{
+  item.addEventListener('click', scrollForClick);
+})
 
-  let img = document.querySelectorAll('.box-imagem2 img');
+function getScrollHref(element){
 
-  setInterval(function(){
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
 
-    img[interval2].style.display = 'none';
-    interval2++;
-    if(interval2 > maxSlider2){
+function scrollForClick(event){
+  event.preventDefault();
+  const to = getScrollHref(event.target) - 100;
 
-      interval2 = 0
-    }
-    img[interval2].style.display = 'block'
+  scrollToPosition(to)
+}
 
-  },2000)
+function scrollToPosition (to){
+  window.scroll({
+    top:to,
+    behavior: "smooth" 
+    })
+}
 
+//Buttons banner
+const bannerItems = document.querySelectorAll('.buttons-banner a[href^="#"]');
+
+bannerItems.forEach(item =>{
+  item.addEventListener('click', scrollForClick);
+})
+
+function getScrollHrefBanner(element){
+
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollForClickBanner(event){
+  event.preventDefault();
+  const to = getScrollHrefBanner(event.target) - 100;
+
+  scrollToPositionBanner(to)
+}
+
+function scrollToPositionBanner (to){
+  window.scroll({
+    top:to,
+    behavior: "smooth" 
+    })
 }
